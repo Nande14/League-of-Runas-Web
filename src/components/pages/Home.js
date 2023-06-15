f51547ee2d60696c75ab2a55481fa39d287a8ab8
woodpeckerduende <110867010+woodpeckerduende@users.noreply.github.com>
2023-06-15 14:33:24 -0300

    Update Home.js
    Código organizado e criação de botões na homepage.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,25 @@
-import styles from './Home.module.css'
-import tela from '../../img/TelaHome.png'
-function Home(){
-return(
-    
-    <section className={styles.home_container}>
-        <img src={tela} alt="Tela home"></img>
-    </section>
-)
+import React from 'react';
+import styles from './Home.module.css';
+import tela from '../../img/TelaHome.png';
 
+function Home() {
+  const handleClick = () => {
+    alert('Agora você será redirecionado para a próxima página!');
+  };
+
+  return (
+    <section className={styles.home_container}>
+      <img src={tela} alt="Tela home" useMap="#imagemMapa" />
+      <map name="imagemMapa">
+        <area
+          shape="rect" coords="140,260,410,270" alt="Área Clicável"
+          onClick={handleClick} style={{ cursor: 'pointer' }} />
+        <area
+        shape="rect" coords= "220,340,365,350" alt="Área Clicável"
+        onClick={handleClick} style={{cursor:'pointer'}} />
+      </map>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
